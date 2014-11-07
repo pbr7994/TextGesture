6fa47555a194766218a59aae8e38f6d905a37b7f
pbr7994 <brup1_13@uni.worc.ac.uk>
2014-11-07 15:44:38 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,10 +7,10 @@ $(document).on("pagecreate","#pageone",function(){
  	});                       
 
 	$('#taptext').on("tap",function(){
-    	alert("Stop tapping!");
+    	$(this).css('color', 'red');
  	}); 
 
-	$('#swipetext').on("swipeleft",function(){
+	$('#swipetext').on("swiperight",function(){
     	$(this).css('color', 'green');
   	});   
 
